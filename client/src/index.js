7a2c5e2abaa8984206bd86890d073ba04a8365af
AvishkaWPA <avishkabiz123@gmail.com>
2024-06-09 02:07:33 +0530

    Added prettier and other configurations

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {configureStore } from "@reduxjs/toolkit";
-import globalReducer from "state";
-import { Provider } from "react-redux";
+import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from 'state';
+import { Provider } from 'react-redux';
 
 const store = configureStore({
   reducer: {
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
